f905ed4a0a14ac48d9d0a3aa20fa98d2bd8c30b7
Fredson Silva <silva.fredson@hotmail.com>
2023-01-26 11:35:25 -0300

    Initial Commit

---
+++ Class_Student_AddApp/Class_Student_AddAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class_Student_AddAppApp.swift
+//  Class_Student_AddApp
+//
+//  Created by Fredson Silva on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Class_Student_AddAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Class_Student_AddApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Class_Student_AddApp
+//
+//  Created by Fredson Silva on 26/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
